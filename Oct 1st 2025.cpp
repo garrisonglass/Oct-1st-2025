236ae5ef7b6730033d60b348a98fc90f8809216d
garrisonglass <garrison.glass@gmail.com>
2025-10-05 15:34:15 -0400

    Notes for class

--- Oct 1st 2025.cpp
+++ Oct 1st 2025.cpp
@@ -9,26 +9,85 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    int GhostHP = 10;
-    int warriorDamage = rand() % 20 + 1;
-    GhostHP -= warriorDamage;
 
-    if (GhostHP <= 0)
-        cout << "The ghost is beaten\n";
-    //if (GhostHP >= 0)
-    //    cout << "The ghost is still there! ooooh\n";
+    int grade;
+    cin >> grade;
+
+/*    if (grade >= 90)
+    {
+        cout << "It's an A\n";
+    }
+    else 
+    {
+        if (grade > 70) 
+        { 
+            cout << "It's a C"; 
+        }
+        else 
+            if (grade >=60)
+            {
+                cout << "You got a D";
+            }
+            else 
+            {
+                cout << "You fail";
+            }
+
+
+    }
+
+    if (grade >= 90)
+    {
+        cout << "It's an A\n";
+    }
+    else if (grade > 70)
+    {
+    cout << "It's a C";
+    }
+    else if (grade >= 60)
+    {
+     cout << "You got a D";
+    }
     else
-        cout << "The ghost is still there! woooo\n";
+    {
+    cout << "You fail";
+    }*/
+
+
+
+   /* double numberator, denomintor;
+
+    cout << "Enter numberator and demoinator";
+    cin >> numberator >> denomintor;
+    if (denomintor)*/
+
+    //int GhostHP = 10;
+    //int warriorDamage = rand() % 20 + 1;
+    //int playerHP = 2;
+    //GhostHP -= warriorDamage;
+
+    //if (GhostHP <= 0)
+    //{
+    //    cout << "The ghost is beaten\n";
+    //}
+    ////if (GhostHP >= 0)
+    ////    cout << "The ghost is still there! ooooh\n";
+    //else
+    //{
+    //    cout << "The ghost is still there! woooo\n";
+    //    cout << "It will attack back\n";
+    //    playerHP -= rand() % 20 + 1;
+    //    cout << "The player remaining HP:" << playerHP << endl;
+    //    if (playerHP <= 0)
+    //    {
+    //        cout << "the PLAYER has fallen\n";
+    //    }
+    //    else 
+    //    {
+    //        cout << "Fight on\n";
+    //    }
+    //}
+
 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
